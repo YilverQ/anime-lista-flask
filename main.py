daa0ee5e474531ca4aa7d08faebc03c9cbdfab79
YilverQ <yilver0906@gmail.com>
2022-04-12 20:50:55 -0400

    Actualización de CSS y rutas Dinámicas en main.py. YQ

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import render_template
 import  os
 app = Flask(__name__)
 
+RUTA = os.getcwd() + "/static/img"
 
 @app.route('/')
 def index():
@@ -11,20 +12,20 @@ def index():
 
 @app.route("/animes")
 def animes():
-	ruta = "C:\\Users\\Yilver\\Documents\\animes_lista\\static\\img\\Posted Animes"
+	ruta = RUTA + "/Posted Animes"
 	contenido = os.listdir(ruta)
 	return render_template("plantilla.html", contenido = contenido, bloque = "Posted Animes")
 
 
 @app.route("/series")
 def series():
-	ruta = "C:\\Users\\Yilver\\Documents\\animes_lista\\static\\img\\Posted Series"
+	ruta = RUTA + "/Posted Series"
 	contenido = os.listdir(ruta)
 	return render_template("plantilla.html", contenido = contenido, bloque = "Posted Series")
 
 @app.route("/peliculas")
 def peliculas():
-	ruta = "C:\\Users\\Yilver\\Documents\\animes_lista\\static\\img\\Posted Peliculas"
+	ruta = RUTA + "/Posted Peliculas"
 	contenido = os.listdir(ruta)
 	return render_template("plantilla.html", contenido = contenido, bloque = "Posted Peliculas")
 
